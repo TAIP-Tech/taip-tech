5ab977ee71aee0538ef96ca65a750cef44684532
TAIP-Tech <mail@taip.kr>
2024-08-30 14:19:11 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
-const { withContentlayer } = require('next-contentlayer2')
-
+const { withContentlayer } = require('next-contentlayer');
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
-})
+});
 
-// You might need to insert additional domains in script-src if you are using external services
+// Content Security Policy
 const ContentSecurityPolicy = `
   default-src 'self';
   script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app analytics.umami.is;
@@ -13,57 +12,32 @@ const ContentSecurityPolicy = `
   media-src *.s3.amazonaws.com;
   connect-src *;
   font-src 'self';
-  frame-src giscus.app
-`
+  frame-src giscus.app;
+`;
 
 const securityHeaders = [
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
-  {
-    key: 'Content-Security-Policy',
-    value: ContentSecurityPolicy.replace(/\n/g, ''),
-  },
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
-  {
-    key: 'Referrer-Policy',
-    value: 'strict-origin-when-cross-origin',
-  },
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
-  {
-    key: 'X-Frame-Options',
-    value: 'DENY',
-  },
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Content-Type-Options
-  {
-    key: 'X-Content-Type-Options',
-    value: 'nosniff',
-  },
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-DNS-Prefetch-Control
-  {
-    key: 'X-DNS-Prefetch-Control',
-    value: 'on',
-  },
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Strict-Transport-Security
-  {
-    key: 'Strict-Transport-Security',
-    value: 'max-age=31536000; includeSubDomains',
-  },
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Feature-Policy
-  {
-    key: 'Permissions-Policy',
-    value: 'camera=(), microphone=(), geolocation=()',
-  },
-]
+  { key: 'Content-Security-Policy', value: ContentSecurityPolicy.replace(/\n/g, '') },
+  { key: 'Referrer-Policy', value: 'strict-origin-when-cross-origin' },
+  { key: 'X-Frame-Options', value: 'DENY' },
+  { key: 'X-Content-Type-Options', value: 'nosniff' },
+  { key: 'X-DNS-Prefetch-Control', value: 'on' },
+  { key: 'Strict-Transport-Security', value: 'max-age=31536000; includeSubDomains' },
+  { key: 'Permissions-Policy', value: 'camera=(), microphone=(), geolocation=()' },
+];
 
-const output = process.env.EXPORT ? 'export' : undefined
-const basePath = process.env.BASE_PATH || undefined
-const unoptimized = process.env.UNOPTIMIZED ? true : undefined
+// Retrieve environment variables
+const output = process.env.EXPORT ? 'export' : undefined;
+const basePath = process.env.BASE_PATH || '';
+const unoptimized = process.env.UNOPTIMIZED === 'true';
 
 /**
- * @type {import('next/dist/next-server/server/config').NextConfig}
+ * @type {import('next').NextConfig}
  **/
 module.exports = () => {
-  const plugins = [withContentlayer, withBundleAnalyzer]
-  return plugins.reduce((acc, next) => next(acc), {
+  const plugins = [withContentlayer, withBundleAnalyzer];
+
+  // Reduce the plugins array to apply all Next.js plugins
+  return plugins.reduce((acc, plugin) => plugin(acc), {
     output,
     basePath,
     reactStrictMode: true,
@@ -73,10 +47,7 @@ module.exports = () => {
     },
     images: {
       remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'picsum.photos',
-        },
+        { protocol: 'https', hostname: 'picsum.photos' },
       ],
       unoptimized,
     },
@@ -86,15 +57,15 @@ module.exports = () => {
           source: '/(.*)',
           headers: securityHeaders,
         },
-      ]
+      ];
     },
     webpack: (config, options) => {
       config.module.rules.push({
         test: /\.svg$/,
         use: ['@svgr/webpack'],
-      })
+      });
 
-      return config
+      return config;
     },
-  })
-}
+  });
+};
